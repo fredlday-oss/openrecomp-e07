8965d783ead9c747d139bfb810459c3301523065
fredlday-oss <243643093+fredlday-oss@users.noreply.github.com>
2026-09-02 08:04:20 +0100

    unreal: add packaged proof subsystem startup hook

--- integrations/unreal/Plugins/OpenRecompRuntime/Source/OpenRecompRuntime/Public/OpenRecompSubsystem.h
+++ integrations/unreal/Plugins/OpenRecompRuntime/Source/OpenRecompRuntime/Public/OpenRecompSubsystem.h
@@ -12,6 +12,7 @@ class OPENRECOMPRUNTIME_API UOpenRecompSubsystem : public UGameInstanceSubsystem
     GENERATED_BODY()
 
 public:
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
     virtual void Deinitialize() override;
 
     UFUNCTION(BlueprintCallable, Category="OpenRecomp")
